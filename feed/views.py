ad7518f3268578b49eed3b6fab066836267614e3
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-06 11:49:31 -0700

    10. Your first template

--- feed/views.py
+++ feed/views.py
@@ -5,7 +5,7 @@ from .models import Post
 
 class HomePage(ListView):
     http_method_names = ["get"]
-    template_name = "homepage.html"
+    template_name = "feed/homepage.html"
     model = Post
     context_object_name = "posts"
     queryset = Post.objects.all().order_by('-id')[0:30]
