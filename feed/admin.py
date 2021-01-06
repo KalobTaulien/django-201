f628c819add4c008f05c0ecc238456ff99e47c5b
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-06 11:32:33 -0700

    6. Register Post with the Django admin

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
--- feed/models.py
+++ feed/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Post(models.Model):
     text = models.CharField(max_length=240)
 
+    def __str__(self):
+        return self.text[0:100]
