9bd36e0d70a69c894ded61489b8da3138265e7d8
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-06 11:43:53 -0700

    8. Creating a HomePage ListView

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class HomePage(ListView):
+    http_method_names = ["get"]
+    template_name = "homepage.html"
+    model = Post
+    context_object_name = "posts"
+    queryset = Post.objects.all().order_by('-id')[0:30]
--- til/urls.py
+++ til/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from feed import urls as feed_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include(feed_urls, namespace="feed"))
 ]
