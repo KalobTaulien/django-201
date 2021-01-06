2d86ca81a78a8d44194e7d76977ea9cbf5bbf7ce
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-06 11:35:28 -0700

    7. Added a DateTimeField to Posts

--- feed/models.py
+++ feed/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.CharField(max_length=240)
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.text[0:100]
