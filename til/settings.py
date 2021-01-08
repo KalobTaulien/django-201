10d6814b86f8b551399819c1b823513fc111bb4a
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-08 09:50:34 -0700

    21. Adding media settings

--- til/settings.py
+++ til/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
--- til/urls.py
+++ til/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from feed import urls as feed_urls
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(feed_urls, namespace="feed")),
     url("", include("allauth.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
