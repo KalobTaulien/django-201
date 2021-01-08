ea909f27dcde118d7c3f9ddb9214ad1018f5406e
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-08 12:51:30 -0700

    42. Dynamic homepage posts

--- feed/views.py
+++ feed/views.py
@@ -1,17 +1,36 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView, TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
+from followers.models import Follower
+
 from .models import Post
 
 
-class HomePage(ListView):
+class HomePage(TemplateView):
     http_method_names = ["get"]
     template_name = "feed/homepage.html"
-    model = Post
-    context_object_name = "posts"
-    queryset = Post.objects.all().order_by('-id')[0:30]
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        if self.request.user.is_authenticated:
+            following = list(
+                Follower.objects.filter(followed_by=self.request.user).values_list('following', flat=True)
+            )
+            if not following:
+                # Show the default 30
+                posts = Post.objects.all().order_by('-id')[0:30]
+            else:
+                posts = Post.objects.filter(author__in=following).order_by('-id')[0:60]
+        else:
+            posts = Post.objects.all().order_by('-id')[0:30]
+        context['posts'] = posts
+        return context
 
 
 class PostDetailView(DetailView):
