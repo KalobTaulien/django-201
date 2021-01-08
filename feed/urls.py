cda350c45dbc8f39c1e36990fcb4d1a7fc0e06ff
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-08 10:29:17 -0700

    27. Creating your first CreateView

--- feed/urls.py
+++ feed/urls.py
@@ -6,5 +6,6 @@ app_name = "feed"
 
 urlpatterns = [
     path("", views.HomePage.as_view(), name="index"),
-    path("<int:pk>/", views.PostDetailView.as_view(), name="detail")
+    path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
+    path("new/", views.CreateNewPost.as_view(), name="new_post"),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .models import Post
 
@@ -16,3 +17,9 @@ class PostDetailView(DetailView):
     template_name = "feed/detail.html"
     model = Post
     context_object_name = "post"
+
+
+class CreateNewPost(CreateView):
+    model = Post
+    template_name = "feed/create.html"
+    fields = ['text']
