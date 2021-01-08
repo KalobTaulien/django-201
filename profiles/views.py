46b76d4f267c06995afc5bd8229000892167d346
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-08 11:59:05 -0700

    37. Adding total posts to profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
 
+from feed.models import Post
+
 
 class ProfileDetailView(DetailView):
     http_method_names = ["get"]
@@ -9,3 +11,10 @@ class ProfileDetailView(DetailView):
     context_object_name = "user"
     slug_field = "username"
     slug_url_kwarg = "username"
+
+    def get_context_data(self, **kwargs):
+        user = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context['total_posts'] = Post.objects.filter(author=user).count()
+        # context['total_followers'] = ...
+        return context
