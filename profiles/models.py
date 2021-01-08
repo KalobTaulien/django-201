8f1e0e812b2ffc339605a4d3ae6786a2e0ea2473
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-08 09:44:02 -0700

    20. Adding a 3rd party image thumbnail package

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from sorl.thumbnail import ImageField
 
 
 class Profile(models.Model):
@@ -10,6 +11,7 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         related_name="profile"
     )
+    image = ImageField(upload_to='profiles')
 
     def __str__(self):
         return self.user.username
--- til/settings.py
+++ til/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
